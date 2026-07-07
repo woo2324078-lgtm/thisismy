c498880da49ff405d359a4d1987f58ae5574c402
woo2324078-lgtm <woo2324078@dcfwms.edu.hk>
2026-07-07 09:09:20 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
+let light2 = 0
 led.enable(false)
 let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+basic.showNumber(67)
+basic.showString("Minecraft")
 basic.forever(function () {
-	
+    light2 = smarthome.ReadLightIntensity(AnalogPin.P3)
+    if (light2 < 50) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
 })
